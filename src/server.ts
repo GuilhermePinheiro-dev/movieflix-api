c5f6180a88c610235cfb7166749438eb7726e0f4
Guilherme Campos <guicamppos12@gmail.com>
2026-04-20 15:47:38 -0300

    feat: atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -30,8 +30,10 @@ app.post("/movies", async (req, res) => {
             },
         });
 
-        if(movieWithSameTitle){
-            res.status(409).send({message: "Já existe um filme com este titulo"})
+        if (movieWithSameTitle) {
+            res.status(409).send({
+                message: "Já existe um filme com este titulo",
+            });
         }
 
         await prisma.movie.create({
@@ -48,6 +50,33 @@ app.post("/movies", async (req, res) => {
     } catch (error) {
         return res.status(500).send({ message: "Erro ao cadastrar o filme" });
     }
+});
+
+app.put("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: { id },
+        });
+
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado" });
+        }
+
+        const data = { ...req.body };
+        data.release_date = data.release_date
+            ? new Date(data.release_date)
+            : undefined;
+
+        await prisma.movie.update({
+            where: { id },
+            data,
+        });
+    } catch (error) {
+        return res.status(500).send({message: "Falha ao atualizar o registro do filme"})
+    }
+
+    res.status(200).send()
 });
 
 app.listen(port, () => {
